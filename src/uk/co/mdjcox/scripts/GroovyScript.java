59d740eece1ef2d716add75a09c6769db26de225
michaeldjcox <michael.d.j.cox@btinternet.com>
2013-04-02 07:44:57 +0100

    Adding groovy script base methods

---
+++ src/uk/co/mdjcox/scripts/GroovyScript.java
@@ -0,0 +1,11 @@
+package uk.co.mdjcox.scripts;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: michael
+ * Date: 31/03/13
+ * Time: 00:11
+ * To change this template use File | Settings | File Templates.
+ */
+public class GroovyScript {
+}
