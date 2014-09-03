231c0a82b669f5585f7d877b5abbba4f26c97a7d
michael <michael.d.j.cox@btinternet.com>
2014-09-03 13:16:43 +0100

    Add an errors page to the server and rework iplayer script to log errors. Plus some work on fixing iplayer parse errors

---
+++ src/main/java/uk/co/mdjcox/sagetv/model/ErrorRecorder.java
@@ -0,0 +1,14 @@
+package uk.co.mdjcox.sagetv.model;
+
+import java.util.List;
+
+/**
+ * Created by michael on 02/09/14.
+ */
+public interface ErrorRecorder {
+  List<ParseError> getErrors();
+
+  void addError(String level, String plugin, String programme, String episode, String sourceUrl, String message);
+
+  boolean hasErrors();
+}
---
+++ src/main/java/uk/co/mdjcox/sagetv/model/ParseError.java
@@ -0,0 +1,7 @@
+package uk.co.mdjcox.sagetv.model;
+
+/**
+ * Created by michael on 01/09/14.
+ */
+public class ParseError {
+}
