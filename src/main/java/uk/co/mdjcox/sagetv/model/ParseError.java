f9ccae7c2c88a330e362bc7da2490f5dc6c5376d
Michael Cox <michaeldjcox@gmail.com>
2014-11-10 08:11:14 +0000

    Deal with possible nulls

--- src/main/java/uk/co/mdjcox/sagetv/model/ParseError.java
+++ src/main/java/uk/co/mdjcox/sagetv/model/ParseError.java
@@ -51,25 +51,26 @@ public class ParseError implements Comparable<ParseError> {
     return message;
   }
 
+
   @Override
   public boolean equals(Object o) {
     if (this == o) return true;
     if (o == null || getClass() != o.getClass()) return false;
 
     ParseError that = (ParseError) o;
-    if (!level.equals(that.level)) return false;
-    if (!message.equals(that.message)) return false;
+
+    if (item != null ? !item.equals(that.item) : that.item != null) return false;
+    if (level != null ? !level.equals(that.level) : that.level != null) return false;
+    if (message != null ? !message.equals(that.message) : that.message != null) return false;
 
     return true;
   }
 
   @Override
   public int hashCode() {
-    int result = level.hashCode();
-    result = 31 * result + getSource().hashCode();
-    result = 31 * result + getType().hashCode();
-    result = 31 * result + getId().hashCode();
-    result = 31 * result + message.hashCode();
+    int result = item != null ? item.hashCode() : 0;
+    result = 31 * result + (level != null ? level.hashCode() : 0);
+    result = 31 * result + (message != null ? message.hashCode() : 0);
     return result;
   }
 
