5e0839bd9ca0bac0e606f0deb8ea6b3c89f41993
Michael Cox <michaeldjcox@gmail.com>
2014-11-20 13:18:12 +0000

    Fix debug output

--- src/main/java/uk/co/mdjcox/sagetv/catchup/Cataloger.java
+++ src/main/java/uk/co/mdjcox/sagetv/catchup/Cataloger.java
@@ -731,7 +731,7 @@ public class Cataloger {
         logger.info("Initial catalog looks a bit sparse: " + currentCount + " programmes when expected at least " + threshold + " - cataloging now");
         catalogingScheduler.schedule(runnable, 0, TimeUnit.MILLISECONDS);
       } else {
-        logger.info("Initial catalog looks populated - next catalog in " + String.format("Finished in %02d:%02d:%02d", hours, minutes, seconds));
+        logger.info("Initial catalog looks populated - next catalog in " + String.format("%02d:%02d:%02d", hours, minutes, seconds));
       }
 
       future = catalogingScheduler.scheduleAtFixedRate(runnable, initialDelay, refreshRate * 60 * 60 * 1000, TimeUnit.MILLISECONDS);
